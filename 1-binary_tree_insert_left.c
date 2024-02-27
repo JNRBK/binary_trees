f9874f8399124fb79a22b9020a128432c8525f10
Yara-ib <yr.ap.23@gmail.com>
2024-02-27 22:29:17 +0200

    Edit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,7 +27,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		node->left = parent->left;
 		parent->left->parent = node;
 	}
-	parent->left = node;
 
-	return (parent);
+	parent->left = node;
+	return (node);
 }
