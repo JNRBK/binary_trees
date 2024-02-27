92d0beb50f72a9c3693145d53b5431737309fcd2
JNRBK <rafeekmorgan@yahoo.com>
2024-02-28 01:07:40 +0200

    take it or leaf it

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,10 +11,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if (node->parent && node->left == NULL && node->right == NULL)
-		return (1);
-
-	if (node->left && node->left->left == NULL && node->left->right == NULL)
+	if (node->right != NULL || node->left != NULL)
+		return (0);
+	else
 		return (1);
 
 	return (0);
