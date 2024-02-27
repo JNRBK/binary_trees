33a6edbf81eb5e7655df149a84a2e42e50445137
Yara-ib <yr.ap.23@gmail.com>
2024-02-27 22:26:33 +0200

    Edit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,9 +9,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (!parent)
+		return (NULL);
+
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-	if (!node || !parent)
+	if (!node)
 		return (NULL);
 
 	node->parent = parent;
