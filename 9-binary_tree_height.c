890c44852f2daecda7d6364aa2ed6c0c11cc017e
Yara-ib <yr.ap.23@gmail.com>
2024-02-28 14:06:01 +0200

    Recursion ~

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,24 +18,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		/* Adding extra count because of the root node itself */
 		/* The recursive call will handle all the left nodes within */
 		count_left = 1 + binary_tree_height(tree->left);
-		/* Checking the other side of the subtree, the right leaves */
-		if (tree->left->right)
-			count_left += binary_tree_height(tree->left->right);
 	}
-	else
-		count_left = 0;
 
 	if (tree->right)
 	{
 		/* Adding extra count because of the root node itself */
 		/* The recursive call will handle all the right nodes within */
 		count_right = 1 + binary_tree_height(tree->right);
-		/* Checking the other side of the subtree, the left leaves */
-		if (tree->right->left)
-			count_right += binary_tree_height(tree->right->left);
 	}
-	else
-		count_right = 0;
 
 	/* Comparing which side is the longest */
 	if (count_left < count_right)
