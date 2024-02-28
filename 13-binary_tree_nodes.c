69549da346aada10b7a824953b66860699b94abc
JNRBK <rafeekmorgan@yahoo.com>
2024-02-28 21:40:03 +0200

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,11 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
 	if (!tree)
 		return (0);
 
 	if (!tree->left && !tree->right)
-		return (count);
+		return (0);
 
 	if (tree->left || tree->right)
 		return 1 + binary_tree_nodes(tree->left) +
