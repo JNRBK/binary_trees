a6272597e108fef9a6bd51a8309b8202c43e8686
JNRBK <rafeekmorgan@yahoo.com>
2024-02-28 20:22:51 +0200

    leaf

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,8 +10,12 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left || !tree->right)
+		return (1);
+
 	if (tree->left && tree->right)
-		return (2);
+		return binary_tree_leaves(tree->left) +
+	binary_tree_leaves(tree->right);
 
 	return (1);
 }
