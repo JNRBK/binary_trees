f34716978979cf1a7bdf181ab8a1f346440837a2
Yara-ib <yr.ap.23@gmail.com>
2024-02-28 03:11:36 +0200

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,21 +8,22 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int count = 0;
+	size_t count = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
 	{
 		count += 1;
 		binary_tree_height(tree->left);
-	}
-
-	if (tree->right)
-	{
-		count += 1;
-		binary_tree_height(tree->right);
+		{
+			if (tree->left->right)
+			{
+				count += 1;
+				binary_tree_height(tree->left->right);
+			}
+		}
 	}
 
 	return (count);
