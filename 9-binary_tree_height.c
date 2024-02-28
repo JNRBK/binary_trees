e4fce74ca9bc8cfd6f496dd2b3e7b85c7ed4eedc
Yara-ib <yr.ap.23@gmail.com>
2024-02-28 02:35:09 +0200

    Is that you?

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,14 +15,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree->left)
 	{
-		binary_tree_height(tree->left);
 		count += 1;
+		binary_tree_height(tree->left);
 	}
 
 	if (tree->right)
 	{
-		binary_tree_height(tree->right);
 		count += 1;
+		binary_tree_height(tree->right);
 	}
 
 	return (count);
