b215a695fcbfe25fc3c22504bb9291bfeb42597b
Yara-ib <yr.ap.23@gmail.com>
2024-03-01 04:55:59 +0200

    Ya RaB

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,10 +11,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent->left)
+	if (node->parent->left == node)
 		return (node->parent->right);
 
-	if (node->parent->right)
+	if (node->parent->right == node)
 		return (node->parent->left);
 
 	return (NULL);
