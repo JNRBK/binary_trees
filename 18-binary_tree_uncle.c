1285eee990f51623aa4cdfe0098d144c82e48dc8
Yara-ib <yr.ap.23@gmail.com>
2024-03-02 03:12:05 +0200

    BN_UN

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node.
+ * @node: pointer to the node to find the uncle.
+ * Return: NULL || pointer to the uncle node.
+*/
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	/* NODE has only 2 childs / one level || No tree*/
+	if (!node)
+		return (NULL);
+
+	if (node->parent->parent)
+	{
+		/* ROOT > RIGHT */
+		if (node->parent->parent->right->left == node)
+			return (node->parent->parent->left);
+
+		if (node->parent->parent->right->right == node)
+			return (node->parent->parent->left);
+
+		/* ROOT > LEFT */
+		if (node->parent->parent->left->left == node)
+			return (node->parent->parent->right);
+
+		if (node->parent->parent->left->right == node)
+			return (node->parent->parent->right);
+	}
+	return (NULL);
+}
